23e6e40c525bdb813d75b10a173700de05ba1f81
jpmac <jpmac@jpmacdeMacBook-Pro.local>
2017-02-22 14:29:17 +0800

    Initial Commit

---
+++ etool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  etool
+//
+//  Created by jpmac on 2017/2/22.
+//  Copyright © 2017年 yidont. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
